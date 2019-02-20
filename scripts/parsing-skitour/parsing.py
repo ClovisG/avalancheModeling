a8a177c9c7a6a478f70c5dc8c6a9a1a8e6c7c7e1
Théo Larue <theo.larue@grenoble-inp.org>
2019-02-20 23:40:59 +0100

    Coordonnées de départ

--- scripts/parsing-skitour/parsing.py
+++ scripts/parsing-skitour/parsing.py
@@ -1,6 +1,6 @@
 """
 Web scraping of skitour.com, in order to find data on outings having a negative avalanch activity.
-Gets data like location (approximate), date, inclination
+Gets data like location (approximate), date, inclination, orientation.
 
 Created on 18/02/19
 Author : Théo Larue
@@ -9,6 +9,9 @@ Author : Théo Larue
 from bs4 import BeautifulSoup # Parsing library for html files
 import requests # Enable us to use html resquests
 import re # Regular expressions)
+import logging, sys # For debugging information
+
+logging.basicConfig(stream = sys.stderr, level=logging.DEBUG)
 
 last_page = 121 # TODO : find automatically the number of pages
 
@@ -18,6 +21,8 @@ URL = 'http://www.skitour.fr/conditions/'
 parsed_events = []
 
 # Key words used to determine if there was an avalanch activity
+# Idea : usage of a python library in order to match words close to the ones referenced in the
+# txt file, in case of typos for example
 with open("mots-cles-neg.txt") as f:
     key_words = f.read().splitlines()
 
@@ -47,7 +52,9 @@ def find_negatives(last_page, key_words):
                 match = re.search('Activité avalancheuse observée :(.*)Skiabilité', outing.text)
                 # Check if the activity is negative
                 if(match != None and any(word in match.group(1) for word in key_words)):
-                    parsed_events.append(parse_outing(outing, ref))
+                    logging.info("Parsing {}".format(ref))
+                    parsed_events.append(parse_outing(ref))
+                    logging.info("Done.\n")
 
 """
 Parses the outing from the website into a dictionnary, whose fields describe the activity
@@ -60,7 +67,7 @@ Arguments :
 Returns :
     A dictionnary containing some data found on the page.
 """
-def parse_outing(outing, ref):
+def parse_outing(ref):
     data = {}
 
     req = requests.get("{}{}".format(URL, ref))
@@ -72,18 +79,47 @@ def parse_outing(outing, ref):
     # The general informations are situated in the first "table" tag
     outing_data = soup.find("table")
     massif = outing_data.find("strong",text="Massif : ").nextSibling
-    secteur = outing_data.find("strong",text="Secteur : ").nextSibling
     orientation = outing_data.find("strong",text="Orientation : ").nextSibling
     data["massif"] = massif
-    data["secteur"] = secteur
     data["orientation"] = orientation
+    # Sometimes the sector is not specified
+    secteur = outing_data.find("strong",text="Secteur : ")
+    if(secteur != None):
+        data["secteur"] = secteur.nexSibling
+    else:
+        data["secteur"] = ""
+        logging.info("No sector data for {}".format(ref))
 
+    # Parsing of the gps coordinates
+    link_to_location = outing_data.find("strong",text="Départ : ").findNext("a")
+    coordinates = parse_starting_point(link_to_location["href"])
+    data["depart"] = coordinates
+
+    logging.debug(data)
     return data
 
 """
 Parses a page describing a location, and returns the gps coordinates of that location
 """
 def parse_starting_point(ref):
-    pass
+    req = requests.get("{}{}".format(URL, ref))
+    soup = BeautifulSoup(req.text, "lxml")
+    coords = {}
+
+    coordinates = soup.find("abbr", {"title": "WGS84"})
+    if(coordinates != None):
+        coordinates = coordinates.nextSibling
+        logging.debug("Coordonnees : {}".format(coordinates))
+        coords["lat"] = re.search(" : (.*) N", coordinates).group(1)
+        coords["lon"] = re.search(" / (.*) E", coordinates).group(1)
+        logging.debug("Latitude : {}".format(coords["lat"]))
+        logging.debug("Longitude : {}".format(coords["lon"]))
+    else:
+        logging.info("No coordinates for {}".format(ref))
+        coords["lat"] = ""
+        coords["lon"] = ""
+
+    return coords
+
 
 find_negatives(last_page, key_words)
