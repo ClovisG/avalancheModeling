e271053a2e5f8d944700aab109b59347a9c10032
Théo Larue <theo.larue@grenoble-inp.org>
2019-02-21 00:06:46 +0100

    Parsing du nombre de pages

--- scripts/parsing-skitour/parsing.py
+++ scripts/parsing-skitour/parsing.py
@@ -13,8 +13,6 @@ import logging, sys # For debugging information
 
 logging.basicConfig(stream = sys.stderr, level=logging.DEBUG)
 
-last_page = 121 # TODO : find automatically the number of pages
-
 # Adress where you can find the outings
 URL = 'http://www.skitour.fr/conditions/'
 
@@ -26,6 +24,19 @@ parsed_events = []
 with open("mots-cles-neg.txt") as f:
     key_words = f.read().splitlines()
 
+
+"""
+Returns the number of the last html page of the website. Used in 'find_negatives'
+"""
+def find_last_page():
+        req = requests.get(URL)
+        soup = BeautifulSoup(req.text, "lxml")
+
+        n = int(soup.find("p", {"class": "centre"}).contents[-3].text)
+        logging.debug("Number of pages : {}\n".format(n))
+        return n
+
+
 """
 Finds all the fields that represent a negative value (relative to avalanch activity) and
 parses the field into a list of dictionaries
@@ -36,15 +47,15 @@ because of the "rien" key word, which is not what we want to recognize.
 Arguments :
         last_page : number of the last page of the website
         key_words : the words used to determine if the activity was negative
+        parsed_events : the list where the parsed events are to be stored
 """
-def find_negatives(last_page, key_words):
-    # This loop gets the contents of the avalanche activity section of the tour reports
+def find_negatives(last_page, key_words, parsed_events):
+    # There are a lot of pages, and on every page, a few ski outings to parse
     for i in range(1, last_page+1):
-        # Download of the web page
         req = requests.get("{}?p={}".format(URL, i))
-        # Parsing of the page using lxml parser
         soup = BeautifulSoup(req.text, "lxml")
         outings = soup.find("div", {"id": "cont"}).find("div", {"class": "cadre"}, recursive = False)
+
         for title in outings.findChildren("h4", recursive = False):
             ref = title.find("a")["href"] # Adress to the actual content of the outing
             outing = title.findNext("ul")
@@ -56,12 +67,12 @@ def find_negatives(last_page, key_words):
                     parsed_events.append(parse_outing(ref))
                     logging.info("Done.\n")
 
+
 """
 Parses the outing from the website into a dictionnary, whose fields describe the activity
 (date, approximate location, value of the inclination)
 
 Arguments :
-    outing : BeautifulSoup object representing the negative activity detected
     ref : reference to the content to be parsed
 
 Returns :
@@ -98,8 +109,15 @@ def parse_outing(ref):
     logging.debug(data)
     return data
 
+
 """
 Parses a page describing a location, and returns the gps coordinates of that location
+
+Arguments :
+    ref : html reference to the page needing parsing
+
+Returns :
+    A dictionnary containing the latitude and longitude of the location
 """
 def parse_starting_point(ref):
     req = requests.get("{}{}".format(URL, ref))
@@ -122,4 +140,4 @@ def parse_starting_point(ref):
     return coords
 
 
-find_negatives(last_page, key_words)
+find_negatives(find_last_page(), key_words, parsed_events)
