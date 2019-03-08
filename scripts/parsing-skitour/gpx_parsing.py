dc70a8a6dd8b14d6a5f29819628a1c955e83f2eb
Théo Larue <theo.larue@grenoble-inp.org>
2019-03-08 16:20:56 +0100

    ajout d'un script pour parser les .gpx

---
+++ scripts/parsing-skitour/gpx_parsing.py
@@ -0,0 +1,57 @@
+"""
+Test script for quick gps parsing, in order to get the position of the slopes from a gpx file
+Computes the mean coordinates of the gps trace
+"""
+
+import gpxpy
+import logging, sys # For debugging information
+
+# Comment or uncomment if you want debug information
+logging.basicConfig(stream = sys.stderr, level=logging.DEBUG)
+
+"""
+Computes the mean coordinates and elevation of the given .gpx file
+"""
+def mean_coordinates(filename):
+    logging.info("file : {}".format(filename))
+
+    lat_mean = 0
+    long_mean = 0
+    alt_mean = 0
+    n = 0 # Number of points in the route
+
+    with open(filename,'r') as fin:
+        gpx = gpxpy.parse(fin)
+        points = []
+
+        routes_nb = len(gpx.routes)
+        if(routes_nb > 0):
+            points = gpx.routes[0].points
+            if(routes_nb > 1):
+                logging.warning("{} routes found in the gpx file".format(routes_nb))
+
+        tracks_nb = len(gpx.tracks)
+        if(tracks_nb > 0):
+            points = gpx.tracks[0].segments[0].points
+            if(tracks_nb > 1):
+                logging.warning("{} tracks found in the gpx file".format(tracks_nb))
+            if(len(gpx.tracks[0].segments) > 1):
+                logging.warning("{} segments found in the track".format(len(gpx.tracks[0].segments)))
+
+        n = len(points)
+        logging.info("Number of points : {}".format(n))
+
+        for point in points:
+            lat_mean += point.latitude
+            long_mean += point.longitude
+            alt_mean += point.elevation
+
+        lat_mean /= n
+        long_mean /= n
+        alt_mean /= n
+        logging.debug("Moyennes : lat {} long {} alt {}".format(lat_mean, long_mean, alt_mean))
+
+        return lat_mean, long_mean, alt_mean
+
+mean_coordinates('test-single-rte.gpx')
+mean_coordinates('test-trk.gpx')
