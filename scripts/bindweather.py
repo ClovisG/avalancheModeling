9ac98e9e0aa408369d4e6798c3fd16a6b21cc85d
Martin Herault <martin.herault@grenoble-inp.org>
2019-03-09 18:06:33 +0100

    Script pour collecter les données correspondant aux evenements positifs de avalanches.xls via des requêtes cdsapi sur copernicus

---
+++ scripts/bindweather.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+import cdsapi
+c = cdsapi.Client()
+
+from pandas import DataFrame, read_csv
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+
+
+def normaliseDate(date):
+    day = date[0:2]
+    month = date[3:5]
+    year = date[6:]
+    return [day,month,year]
+
+def normaliseHeure(heure):
+    if (heure == ""):
+        return '12:00'
+    else:
+        return heure[:3]+'00'
+
+def normaliseNeige(neige):
+    if(neige == ""):
+        return 'undefined'
+    else:
+        return neige
+
+
+# with open('avalanchesWeather.csv', mode='w') as outputFile:
+#     dataWriter = csv.writer(outputFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+
+# dataWriter.writerow(['Id', 'Orientation', 'Date', 'Heure', 'Neige', 'Lat', 'Long',
+#             '2m_temperature','clear_sky_direct_solar_radiation_at_surface',
+#             'large_scale_precipitation','large_scale_snowfall','snow_density','snow_depth',
+#             'snowmelt'])
+
+    # employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    # employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+
+
+inputFile = r'../data/avalanches_2018.10.10-17.12.xls'
+df = pd.read_excel(inputFile)
+
+
+# remove messy data
+df = df[df['Decl a distance'] != 'true']
+
+
+for i in range(1,10):
+    print(i)
+    id = df['Id'][i]
+    orientation = df['Orientation'][i]
+    date = df['Date'][i]
+    heure = str(df['Heure'][i])
+    neige = df['Qualite neige'][i]
+    lat = df['latitude'][i]
+    lon = df['longitude'][i]
+
+    [day, month, year] = normaliseDate(date)
+
+    heure = normaliseHeure(heure)
+
+    print(heure)
+    print(day)
+    print(month)
+    print(year)
+
+
+
+    c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type':'reanalysis',
+        'format':'netcdf',
+        'variable':[
+            '2m_temperature','clear_sky_direct_solar_radiation_at_surface','large_scale_precipitation',
+            'large_scale_snowfall','snow_density','snow_depth',
+            'snowmelt'
+        ],
+        'year':year,
+        'month':month,
+        'day':day,
+        'time':heure
+    },
+    '../download/download'+str(i)+'.nc')
