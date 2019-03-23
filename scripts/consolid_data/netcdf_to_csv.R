029500dab7d4da511b40de2b13330b96376f1d38
Mame Ousmane DIOP <mame-ousmane.diop@grenoble-inp.org>
2019-03-23 21:27:14 +0100

    From nc to csv(data frame) ok

--- scripts/consolid_data/netcdf_to_csv.R
+++ scripts/consolid_data/netcdf_to_csv.R
@@ -1,4 +1,5 @@
-# TODO : comment the code
+# From netcdf file to data frame store in csv file
+
 library(RNetCDF)
 
 # Build a data frame from a NetCDF and store the data frame like a csv file
@@ -9,24 +10,31 @@ nc_to_csv<-function(ncin) {
   lat <-  lat <- var.get.nc(ncin, 1)
   names <- c(names, var.inq.nc(ncin, 1)$name)
   data <-  as.matrix(expand.grid(long,lat))
-  
   nvars <- file.inq.nc(ncin)$nvar
   for (var in 3:(nvars-1)) {
     var_data <- var.get.nc(ncin, var)
     names <- c(names, var.inq.nc(ncin, var)$name)
     data <- cbind(data, as.vector(var_data))
   }
-  
+  print(dim(data))
   data_fram <- as.data.frame(data)
   names(data_fram) <- names
   
-  csv_path <- "./" # TODO : find the right path
-  csv_name <- "test.csv" # TODO : find the right name
+  csv_path <- "~/2A_cours/Avanlaches/scripts/consolid_data/" 
+  csv_name <- "test.csv"
   csv_file <- paste(csv_path, csv_name, sep = "")
-  write.table(na.omit(data_fram),csv_file, row.names=FALSE, sep=",")
+  write.table(na.omit(data_fram),csv_file, row.names=FALSE, sep=" ")
 }
 
-# TODO : tests
+#test
 
 # open a netcdf file 
-ncin <- open.nc("../data/forecast/adaptor.mars.internal-1552154124.7698312-30208-13-7fa26858-6cdf-4628-903f-11fbc0e6b1bb.nc")
+ncin <- open.nc("~/2A_cours/Avanlaches/scripts/consolid_data/test.nc")
+
+nc_to_csv(ncin)
+
+# class data frame 
+data<-read.csv(file = "~/2A_cours/Avanlaches/scripts/consolid_data/test.csv", sep = " ")
+
+# print the firs line
+data[1,]
