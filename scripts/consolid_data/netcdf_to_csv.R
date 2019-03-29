feb95bbc9f980109264ab565a9fecc9a86d9ce41
Martin Herault <martin.herault@grenoble-inp.org>
2019-03-29 18:22:24 +0100

    weather ok

--- scripts/consolid_data/netcdf_to_csv.R
+++ scripts/consolid_data/netcdf_to_csv.R
@@ -37,99 +37,124 @@
 # # print the firs line
 # data[1,]
 
+#Get a data depending on (lon,lat,day)
+#format: "month/day/year"
+getData<-function(lon, lat, day, df){
+  if (lon>13.75 || lon<5.25){
+    cat("Error: longitude out of range \n")
+    break;
+  }
+  if (lat>49.25 || lat<40.5){
+    cat("Error: latitude out of range \n")
+    break;
+  }
+  days = getNbDays(day)
+  if(days < 1 || days > 5943){
+    cat("Error: date out of range \n")
+  }
+  df[16*(13.75-5)*(49.25-lat)+4*(lon-5.25)+1,days+3]
+}
 
-setwd("Documents/2A/Avalanches/projet")
-library(ncdf4)
-nc <- nc_open("download/weather.nc")
-print(nc)
+#format: "month/day/year"
+getNbDays<-function(date){
+  return (dates(c(date))-dates(c("01/01/1991")))
+}
 
-# Variables
-t2m <- "t2m"
-cdir <- "cdir"
-lsp <- "lsp"
-lsf <- "lsf"
-rsn <- "rsn"
-sd <- "sd"
-smlt <- "smlt"
+writeCsv<-function(df){
+  # write out the dataframe as a .csv file
+  csvpath <- ""
+  csvname <- "weather.csv"
+  csvfile <- paste(csvpath, csvname, sep="")
+  print(csvfile)
+  write.table(na.omit(df),csvfile, sep=", ",row.names=FALSE)
+}
 
-#get longitude
-lon <- ncvar_get(nc, "longitude")         
-nlon <- dim(lon)
-head(lon)
+getDataframe<-function(var, time, lon, lat){
+  # get var
+  var_array <- ncvar_get(nc,var)
+  varlname <- ncatt_get(nc,var,"long_name")
+  dunits <- ncatt_get(nc,var,"units")
+  fillvalue <- ncatt_get(nc,var,"_FillValue")
+  dim(t2m_array) #lon*lat*time
+  
+  nt<- dim(time)
+  head(time)
+  
+  # replace netCDF fill values with NA's
+  var_array[t2m_array==fillvalue$value] <- NA
+  
+  length(na.omit(as.vector(var_array[,,1])))
+  
+  # create dataframe -- reshape data
+  # reshape the array into vector
+  var_vec_long <- as.vector(var_array)
+  length(var_vec_long)
+  
+  # reshape the vector into a matrix
+  var_mat <- matrix(var_vec_long, nrow=nlon*nlat, ncol=nt)
+  dim(var_mat)
+  
+  # create a dataframe
+  lonlat <- as.matrix(expand.grid(lon,lat))
+  var_df <- data.frame(cbind(lonlat,t2m_mat))
+  
+  return (var_df)
+}
 
-#get latitude
-lat <- ncvar_get(nc, "latitude")
-nlat <- dim(lat)
-head(lat)
+getFrames<-function(){
+  #setwd("Documents/2A/Avalanches/projet")
+  # load some packages
+  library(chron)
+  library(lattice)
+  library(RColorBrewer)
+  library(ncdf4)
+  nc <- nc_open("download/weather.nc")
+  print(nc)
+  
+  # Variables
+  t2m <- "t2m"
+  cdir <- "cdir"
+  lsp <- "lsp"
+  lsf <- "lsf"
+  rsn <- "rsn"
+  sd <- "sd"
+  smlt <- "smlt"
+  
+  #get longitude
+  lon <- ncvar_get(nc, "longitude")         
+  nlon <- dim(lon)
+  head(lon)
+  
+  #get latitude
+  lat <- ncvar_get(nc, "latitude")
+  nlat <- dim(lat)
+  head(lat)
+  
+  print(c(nlon,nlat))
+  
+  #get time
+  time <- ncvar_get(nc, "time")
+  tunits <- ncatt_get(nc,"time","units")
+  nt<- dim(time)
+  head(time)
+  nt
+  tunits
+  
+  nt<- dim(time)
+  head(time)
+  
+  t2m_df = getDataframe(t2m, time, lon, lat)
+  cdir_df = getDataframe(cdir, time, lon, lat)
+  lsp_df = getDataframe(lsp, time, lon, lat)
+  lsf_df = getDataframe(lsf, time, lon, lat)
+  rsn_df = getDataframe(rsn, time, lon, lat)
+  sd_df = getDataframe(sd, time, lon, lat)
+  smlt_df = getDataframe(smlt, time, lon, lat)
 
-print(c(nlon,nlat))
+  return (list("t2m"=t2m_df,"cdir"=cdir_df,"lsp"=lsp_df,"lsf"=lsf_df,"rsn"=rsn_df,"sd"=sd_df,"smlt"=smlt_df))
+}
 
-#get time
-time <- ncvar_get(nc, "time")
-tunits <- ncatt_get(nc,"time","units")
-nt<- dim(time)
-head(time)
-nt
-tunits
+frames=getFrames()
 
-# get temperature
-t2m_array <- ncvar_get(nc,t2m)
-t2mlname <- ncatt_get(nc,t2m,"long_name")
-dunits <- ncatt_get(nc,t2m,"units")
-fillvalue <- ncatt_get(nc,t2m,"_FillValue")
-dim(t2m_array) #lon*lat*time
-
-dname <- "t2m"                         # name of variable which can be found by using print(nc)
-
-nt<- dim(time)
-head(time)
-
-# load some packages
-library(chron)
-library(lattice)
-library(RColorBrewer)
-
-# convert time -- split the time units string into fields
-tustr <- strsplit(tunits$value, " ")
-tdstr <- strsplit(unlist(tustr)[3], "-")
-tmonth <- as.integer(unlist(tdstr)[2])
-tday <- as.integer(unlist(tdstr)[3])
-tyear <- as.integer(unlist(tdstr)[1])
-chron(time,origin=c(tmonth, tday, tyear))
-
-# replace netCDF fill values with NA's
-t2m_array[t2m_array==fillvalue$value] <- NA
-
-length(na.omit(as.vector(t2m_array[,,1])))
-
-
-# get a single slice or layer (January)
-m <- 1
-t2m_slice <- t2m_array[,,m]
-# quick map
-image(lon,rev(lat),t2m_slice, col=rev(brewer.pal(10,"RdBu")))
-
-
-# create dataframe -- reshape data
-# reshape the array into vector
-t2m_vec_long <- as.vector(t2m_array)
-length(t2m_vec_long)
-
-# reshape the vector into a matrix
-t2m_mat <- matrix(t2m_vec_long, nrow=nlon*nlat, ncol=nt)
-dim(t2m_mat)
-
-# create a dataframe
-lonlat <- as.matrix(expand.grid(lon,lat))
-t2m_df <- data.frame(cbind(lonlat,t2m_mat))
-
-# options(width=96)
-head(na.omit(t2m_df, 20))
-
-# write out the dataframe as a .csv file
-csvpath <- ""
-csvname <- "weather.csv"
-csvfile <- paste(csvpath, csvname, sep="")
-print(csvfile)
-write.table(na.omit(t2m_df),csvfile, sep=",",row.names=FALSE)
-
+t2m_df=frames$t2m
+getData(8,49,"01/03/1991",t2m_df)
