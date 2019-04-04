322baa1c20f8b535e1cb033d38e87295940a6d85
Théo Larue <theo.larue@grenoble-inp.org>
2019-04-04 17:55:49 +0200

    obtention de la pente depuis coordonnées, api hébergée en local

--- scripts/elevation/elevation-plot-example.py
+++ scripts/elevation/elevation-plot-example.py
@@ -3,7 +3,7 @@ Created by Théo Larue on 03/04/2019
 
 Script used for testing and playing around with the SRTMv3 data, exploited through the open-elevation
 open source api.
-At the time this code is writtent the api is hosted locally, however it is also available online.
+At the time this code is written the api is hosted locally, however it is also available online.
 (full documentation on https://open-elevation.com)
 """
 
@@ -46,14 +46,13 @@ lon = 6.866667
 # 1 arcsecond is roughly equivalent to 30m on the surface
 # The dataset has a resolution of 1 arcsecond, going below that leads to modelisation problems.
 # In order to get a higher resolution, the data is interpolated.
-radius_arc = 10
+radius_arc = 2
 radius = arcsec_to_deg(radius_arc)
 res_arc = 1
 resolution = arcsec_to_deg(res_arc)
 
 # 3d plotting
-fig = plt.figure()
-ax = plt.axes(projection="3d")
+fig = plt.figure(figsize=plt.figaspect(0.5))
 
 X = np.arange(lat - radius, lat + radius, resolution)
 Y = np.arange(lon - radius, lon + radius, resolution)
@@ -61,20 +60,26 @@ Y = np.arange(lon - radius, lon + radius, resolution)
 logging.info("Downloading the data...")
 ZZ = np.asarray([[get_elevation(x, y, ip) for x in X] for y in Y])
 XX, YY = np.meshgrid(X, Y)
-# surf = ax.plot_surface(XX, YY, Z, cmap='gist_earth')
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-# plt.show()
+
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+surf = ax.plot_surface(XX, YY, ZZ, cmap='gist_earth')
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.title("Original")
 
 logging.info("Interpolation in progress...")
 Z = ZZ.flatten()
-xi = np.linspace(X.min(), X.max(), (len(Z) / 3))
-yi = np.linspace(Y.min(), Y.max(), (len(Z) / 3))
+interpolation_res = len(X) * 3
+xi = np.linspace(X.min(), X.max(), interpolation_res)
+yi = np.linspace(Y.min(), Y.max(), interpolation_res)
 zi = griddata((XX.flatten(), YY.flatten()), Z, (xi[None, :], yi[:, None]), method='cubic')
 xig, yig = np.meshgrid(xi, yi)
+
+ax = fig.add_subplot(1, 2, 2, projection='3d')
 surf = ax.plot_surface(xig, yig, zi, cmap='gist_earth')
 ax.plot([lat], [lon], [get_elevation(lat, lon, ip)], marker='o', markersize=5, color='r')  # center
 fig.colorbar(surf, shrink=0.5, aspect=5)
-ax.set_title('Terrain topography, source : SRTM v3 (Nasa), one side = {}m'.format(radius_arc * 30))
+# ax.set_title('Terrain topography, source : SRTM v3 (Nasa), one side = {}m'.format(radius_arc * 30))
+ax.set_title('Interpolated')
 ax.set_xlabel('Latitude')
 ax.set_ylabel('Longitude')
 ax.set_zlabel('Elevation (m)')
