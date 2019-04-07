3aeba045afe305e84fa7ba2b60c98e7f8730fb3e
Mame Ousmane DIOP <mame-ousmane.diop@grenoble-inp.org>
2019-04-07 21:58:37 +0200

    Update

--- scripts/consolid_data/netcdf_to_frames.R
+++ scripts/consolid_data/netcdf_to_frames.R
@@ -1,42 +1,3 @@
-# From netcdf file to data frame store in csv file
-# library(RNetCDF)
-# Build a data frame from a NetCDF and store the data frame like a csv file
-# nc_to_csv<-function(ncin) {
-#   names <- NULL
-#   long <-  var.get.nc(ncin, 0)
-#   names <- c(names, var.inq.nc(ncin, 0)$name)
-#   lat <-  lat <- var.get.nc(ncin, 1)
-#   names <- c(names, var.inq.nc(ncin, 1)$name)
-#   data <-  as.matrix(expand.grid(long,lat))
-#   nvars <- file.inq.nc(ncin)$nvar
-#   for (var in 3:(nvars-1)) {
-#     var_data <- var.get.nc(ncin, var)
-#     names <- c(names, var.inq.nc(ncin, var)$name)
-#     data <- cbind(data, as.vector(var_data))
-#   }
-#   print(dim(data))
-#   data_fram <- as.data.frame(data)
-#   names(data_fram) <- names
-#   
-#   csv_path <- "~/2A_cours/Avanlaches/scripts/consolid_data/" 
-#   csv_name <- "test.csv"
-#   csv_file <- paste(csv_path, csv_name, sep = "")
-#   write.table(na.omit(data_fram),csv_file, row.names=FALSE, sep=" ")
-# }
-# 
-# #test
-# 
-# # open a netcdf file 
-# ncin <- open.nc("~/2A_cours/Avanlaches/scripts/consolid_data/test.nc")
-# 
-# nc_to_csv(ncin)
-# 
-# # class data frame 
-# data<-read.csv(file = "~/2A_cours/Avanlaches/scripts/consolid_data/test.csv", sep = " ")
-# 
-# # print the firs line
-# data[1,]
-
 #Get a data depending on (lon,lat,day)
 #format: "month/day/year"
 getData<-function(lon, lat, day, df){
@@ -193,7 +154,7 @@ getFrames<-function(){
   library(RColorBrewer)
   library(ncdf4)
   nc <- nc_open("../download/weather.nc")
-  print(nc)
+  #print(nc)
   
   # Variables
   cdir <- "cdir"
